14acd04579632b6d2553c8a1d16dd4ea8734910f
ylose <heungeob1003@gmail.com>
2024-05-20 02:15:52 +0900

    메인 액티비티 코드 작성

--- app/src/main/java/com/example/batteryinfo/MainActivity.kt
+++ app/src/main/java/com/example/batteryinfo/MainActivity.kt
@@ -1,46 +1,88 @@
 package com.example.batteryinfo
 
+import android.content.Intent
+import android.content.IntentFilter
+import android.content.pm.PackageManager
+import android.graphics.BitmapFactory
+import android.os.BatteryManager
+import android.os.Build
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.batteryinfo.ui.theme.BatteryInfoTheme
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.example.batteryinfo.databinding.ActivityMainBinding
+import com.example.batteryinfo.MyReceiver
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            BatteryInfoTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //add......................
+        val permissionLauncher = registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) {
+            if (it.all { permission -> permission.value == true }) {
+                val intent = Intent(this, MyReceiver::class.java)
+                sendBroadcast(intent)
+            } else {
+                Toast.makeText(this, "permission denied...", Toast.LENGTH_SHORT).show()
             }
         }
-    }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+        registerReceiver(null, IntentFilter(Intent.ACTION_BATTERY_CHANGED))!!.apply {
+            when(getIntExtra(BatteryManager.EXTRA_STATUS, -1)){
+                BatteryManager.BATTERY_STATUS_CHARGING -> {
+                    when(getIntExtra(BatteryManager.EXTRA_PLUGGED, -1)){
+                        BatteryManager.BATTERY_PLUGGED_USB -> {
+                            binding.chargingResultView.text = "USB Plugged"
+                            binding.chargingImageView.setImageBitmap(BitmapFactory.decodeResource(
+                                resources, R.drawable.usb
+                            ))
+                        }
+                        BatteryManager.BATTERY_PLUGGED_AC -> {
+                            binding.chargingResultView.text = "AC Plugged"
+                            binding.chargingImageView.setImageBitmap(BitmapFactory.decodeResource(
+                                resources, R.drawable.ac
+                            ))
+                        }
+                    }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    BatteryInfoTheme {
-        Greeting("Android")
+                }
+                else -> {
+                    binding.chargingResultView.text = "Not Plugged"
+                }
+            }
+            val level = getIntExtra(BatteryManager.EXTRA_LEVEL, -1)
+            val scale = getIntExtra(BatteryManager.EXTRA_SCALE, -1)
+            val batteryPct = level / scale.toFloat() * 100
+            binding.percentResultView.text = "$batteryPct %"
+        }
+
+        binding.button.setOnClickListener {
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                if (ContextCompat.checkSelfPermission(
+                        this,
+                        "android.permission.POST_NOTIFICATIONS"
+                    ) == PackageManager.PERMISSION_GRANTED
+                ) {
+                    val intent = Intent(this, MyReceiver::class.java)
+                    sendBroadcast(intent)
+                } else {
+                    permissionLauncher.launch(
+                        arrayOf(
+                            "android.permission.POST_NOTIFICATIONS"
+                        )
+                    )
+                }
+            }else {
+                val intent = Intent(this, MyReceiver::class.java)
+                sendBroadcast(intent)
+            }
+
+        }
+
     }
 }
